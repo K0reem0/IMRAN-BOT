7e1fbd56b09b4c80dcd13efa20d3cf0e2a7a37fb
Imran khan <imranhacks155022@gmail.com>
2025-08-02 11:33:03 -0700

    Add core files and settings

--- settings.js
+++ settings.js
@@ -1,13 +1,13 @@
 const settings = {
-  packname: '𝐉ᴜɴᴇ 𝐌ᴅ',
-  author: 'supreme',
-  botName: "𝐉ᴜɴᴇ 𝐌ᴅ",
-  botOwner: 'Supreme', // Your name
-  ownerNumber: '254798570132', //Set your number here without + symbol.
+  packname: '✨IMRAN BOT✨',
+  author: '👑 Imran Hacks',
+  botName: '🤖 IMRAN BOT',
+  botOwner: 'Imran Hacks', // Your name
+  ownerNumber: '639691465580', // Set your number here without + symbol.
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
-  description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.2.0",
+  commandMode: 'public', // can be 'public' or 'private'
+  description: '💬 A powerful fun WhatsApp MD bot built with ❤️ by Imran Hacks.',
+  version: '2.0.0',
 };
 
 module.exports = settings;
